2f2e8104d40b13d61aadd3c5c981b1c3d6a1d331
zerochae <zerochae@kakao.com>
2021-11-26 15:02:40 +0900

    dart

--- Programmers/src/com/zerochae/kakao/BlindRecruitment2018/Dart_lv1_30_17682.java
+++ Programmers/src/com/zerochae/kakao/BlindRecruitment2018/Dart_lv1_30_17682.java
@@ -4,82 +4,85 @@ import java.util.regex.Pattern;
 
 public class Dart_lv1_30_17682 {
 
-    static int result;
-    static int resultMap[] = new int[3];
-
     public static void main(String[] args) {
 
-        String input = "1S2D*3T";
-
+        String input = "0D0S#10S";
         System.out.println(solution(input));
 
     }
 
     public static int solution(String dartResult) {
 
-        /*
-
-        if value => 숫자면 숫자로
-        else if value => 영어면 더블
-        else value = 특수문자
-
-
-         */
-
-        String score = "^[0-9]*$";
-        String bonus = "^[A-Z]*$";
-        String event = "[#*]";
-
+        int totalResult = 0;
+        int[] result = new int[3];
+        Integer[] score = new Integer[3];
+        String[] bonus = new String[3];
+        String[] event = new String[3];
+        String scoreReg = "^[0-9]*$";
+        String bonusReg = "^[A-Z]*$";
+        String eventReg = "[#*]";
         int index = 0;
 
-        while (index < 3) {
+        for (int i = 0; i < dartResult.length(); i++) {
+            String input = String.valueOf(dartResult.charAt(i));
 
-
-            for (int i = 0; i < dartResult.length(); i++) {
-
-                String input = String.valueOf(dartResult.charAt(i));
-
-                int n = 0;
-
-                if (regex(score, input)) {
-                    n = Integer.parseInt(input);
-                } else if (regex(bonus, input)) {
-                    switch (input) {
-                        case "S":
-                            break;
-                        case "D":
-                            n *= n;
-                            break;
-                        case "T":
-                            n *= n * n;
-                            break;
-                    }
-                } else if (regex(event, input)) {
-                    switch (input) {
-                        case "#":
-                            n *= -1;
-                            break;
-                        case "*":
-
-                            if (index == 0) {
-
-
-                            } else {
-
-                            }
-
-                            break;
+            if (regex(scoreReg, input)) {
+                if (score[0] != null) {
+                    index++;
+                }
+                int num = Integer.parseInt(input);
+                if (Integer.parseInt(input) == 1) {
+                    String test = String.valueOf(dartResult.charAt(i + 1));
+                    if (!regex(bonusReg, test)) {
+                        num = 10;
+                        i++;
                     }
                 }
-                resultMap[index] = n;
+                score[index] = num;
+            } else if (regex(bonusReg, input)) {
+                bonus[index] = input;
+            } else if (regex(eventReg, input)) {
+                event[index] = input;
             }
-            index++;
         }
-        return result;
+        for (int i = 0; i < 3; i++) {
+            if (event[i] == null) {
+                event[i] = "";
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            int tempScore = 0;
+            switch (bonus[i]) {
+                case "S":
+                    tempScore = score[i];
+                    break;
+                case "D":
+                    tempScore = score[i] * score[i];
+                    break;
+                case "T":
+                    tempScore = score[i] * score[i] * score[i];
+                    break;
+            }
+            switch (event[i]) {
+                case "*":
+                    if (i > 0) result[i - 1] *= 2;
+                    tempScore *= 2;
+                    break;
+                case "#":
+                    tempScore *= -1;
+                    break;
+            }
+            result[i] = tempScore;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            totalResult += result[i];
+        }
+        return totalResult;
     }
 
     static boolean regex(String category, String input) {
         return Pattern.matches(category, input);
     }
-
 }
